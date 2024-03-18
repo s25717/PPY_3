2fff1b6ace4722b7178e55670954885bed04b751
s25717 <s25717@pjwstk.edu.pl>
2024-03-18 13:39:19 +0100

    2 task

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ import os
 # Task 1: List Comprehensions
 squares_list = [x**2 for x in range(1, 11)]
 print("Task 1:", squares_list)
+
+# Task 2: Functions
+def generate_squares(start, end):
+    return [x**2 for x in range(start, end)]
+
+print("Task 2:", generate_squares(1, 11))
+
